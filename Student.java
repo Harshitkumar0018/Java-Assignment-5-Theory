21172bcad48f2ffe1d2edfc64c58f0765f925b41
Harshitkumar0018 <hk3188161@gmail.com>
2025-11-17 12:09:42 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,73 @@
+package model;
+
+import java.util.Scanner;
+
+public class Student extends Person {
+
+    private int rollNo;
+    private String course;
+    private double marks;
+    private String grade;
+
+    public Student() {}
+
+    public Student(int rollNo, String name, String email, String course, double marks) {
+        super(name, email);
+        this.rollNo = rollNo;
+        this.course = course;
+        this.marks = marks;
+        calculateGrade();
+    }
+
+    public void inputDetails() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Roll No: ");
+        rollNo = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter Name: ");
+        name = sc.nextLine();
+
+        System.out.print("Enter Email: ");
+        email = sc.nextLine();
+
+        System.out.print("Enter Course: ");
+        course = sc.nextLine();
+
+        System.out.print("Enter Marks: ");
+        marks = sc.nextDouble();
+
+        calculateGrade();
+    }
+
+    public void calculateGrade() {
+        if (marks >= 90) grade = "A+";
+        else if (marks >= 80) grade = "A";
+        else if (marks >= 70) grade = "B";
+        else if (marks >= 60) grade = "C";
+        else grade = "D";
+    }
+
+    public void displayDetails() {
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("Course: " + course);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + grade);
+    }
+
+    @Override
+    public void displayInfo() {
+        displayDetails();
+    }
+
+    public int getRollNo() { return rollNo; }
+    public String getName() { return name; }
+    public double getMarks() { return marks; }
+
+    public String toFileFormat() {
+        return rollNo + "," + name + "," + email + "," + course + "," + marks;
+    }
+}
