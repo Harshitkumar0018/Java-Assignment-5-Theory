36ba97e0389e9150e8ce58c8e90d67679e744b91
Harshitkumar0018 <hk3188161@gmail.com>
2025-11-17 12:10:07 +0530

    Create RecordActions.java

---
+++ RecordActions.java
@@ -0,0 +1,10 @@
+package service;
+
+public interface RecordActions {
+    void addStudent();
+    void deleteStudent();
+    void updateStudent();
+    void searchStudent();
+    void viewAllStudents();
+    void sortByMarks();
+}
